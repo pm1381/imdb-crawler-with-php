3e4fcb2f2e7c24104fc8d5a54e1e9f93559d41d7
parham <Parham_minoo@yahoo.com>
2022-09-11 02:15:48 -0700

    editing config file

--- src/Configs/Config.php
+++ src/Configs/Config.php
@@ -2,6 +2,7 @@
 define('BASE_URI', 'project/imdb-crawler');
 define('HOST_NAME', 'localhost');
 define("CONTROLLER_NAMESPACE", 'App\Controller');
+define('CRAWLER_ON', 'https://www.imdb.com');
 
 //---mongo database---//
 define('MONGO_DB_NAME', '');
