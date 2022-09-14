92ea781fd506c3d26fa05b0229b692af20962dfe
parham <Parham_minoo@yahoo.com>
2022-09-14 09:59:08 -0700

    finishing cast crawl data

--- src/Models/Cast.php
+++ src/Models/Cast.php
@@ -7,9 +7,11 @@ use App\Helpers\Tools;
 class Cast extends Person {
     private string $specialId;
     private array $pictures = [];
-    public function __construct($name, $url, $pictures = [])
+    private string $page;
+
+    public function __construct($url, $name = "",  $pictures = [])
     {
-        $url = Tools::uniteUrls($url);
+        $url = Tools::checkUrlType($url);
         parent::__construct($name, $url);
         $this->setSpeciaId($url);
         $this->setPictures($pictures);
@@ -17,7 +19,7 @@ class Cast extends Person {
 
     /**
      * Get the value of speciaId
-     */ 
+    */ 
     public function getSpeciaId()
     {
         return $this->specialId;
@@ -27,7 +29,7 @@ class Cast extends Person {
      * Set the value of speciaId
      *
      * @return  self
-     */ 
+    */ 
     public function setSpeciaId($specialId)
     {
         $urlArray = explode("/", $specialId);
@@ -37,7 +39,7 @@ class Cast extends Person {
 
     /**
      * Get the value of pictures
-     */ 
+    */ 
     public function getPictures()
     {
         return $this->pictures;
@@ -47,11 +49,55 @@ class Cast extends Person {
      * Set the value of pictures
      *
      * @return  self
-     */ 
+    */ 
     public function setPictures($pictures)
     {
         $this->pictures = $pictures;
+        return $this;
+    }
 
+    public function findCastSchema()
+    {
+        $result = Tools::getFirstMatch('~<script type="application\/ld\+json">(.*)<\/script>~iUs', $this->getPage());
+        $result = json_decode($result, true);
+        $this->setSchemaData($result);
+    }
+
+    private function setSchemaData(array $result)
+    {
+        $this->setUrl($result['url']);
+        $this->setName($result['name']);
+        $this->setPictures([$result['image']]);
+        if (array_key_exists('birthDate', $result)) {
+            $this->setBirthDate($result['birthDate']);
+        }
+        if (array_key_exists('description', $result)) {
+            $this->setDescription($result['description']);
+        }
+    }
+
+    public function getCastData()
+    {
+        $this->setPage(Tools::manageCUrl([], [], DOMAIN . $this->getUrl()));
+        $this->findCastSchema();
+    }
+
+    /**
+     * Get the value of page
+    */ 
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * Set the value of page
+     *
+     * @return  self
+    */ 
+    public function setPage($page)
+    {
+        $this->page = $page;
         return $this;
     }
 }
